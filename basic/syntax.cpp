294cb85320148ef1103c6aa9a77f1451966db80f
cateto <u2skind@gmail.com>
2022-12-10 22:53:40 +0900

    cpp syntax

---
+++ basic/syntax.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+// 1. header file library : let us work with input and output objects, such as " cout "
+
+using namespace std;
+// 2. namespace : we can use names for objects and variables from the standard library
+
+// 3. function : any code inside its curly brackets "{}" will be executed.
+int main(){
+    cout << "Hello World! \n"; // 4. "see-out" : used with "<<" to output/print text
+    cout << "I am learning c++" <<endl;
+    cout << "Test" ;
+    return 0; // 5. ends the main function.
+}
--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main() {
+int main_2() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "My first c++ Program" << std::endl;
     return 0;
